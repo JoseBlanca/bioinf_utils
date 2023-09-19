61e6e3e1c2e50bea576c1f5fedaafd13482655e6
Jose Blanca <jblanca@upv.es>
2023-09-19 10:51:44 +0200

    implement to_mega dist export

--- src/bioinf_utils/distances.py
+++ src/bioinf_utils/distances.py
@@ -1,4 +1,5 @@
 import itertools
+from io import StringIO
 
 import numpy
 import pandas
@@ -34,8 +35,49 @@ class Distances:
             length += 1
         return dists
 
+    def write_mega(self, fhand):
+        fhand.write("\n#MEGA\n!Format DataType=distance;\n\n")
+        for name in self.names:
+            fhand.write(f"#{name}\n")
+        fhand.write("\n")
+
+        sep = "   "
+        for idx, row in enumerate(self.square_dists.values):
+            row = row[:idx]
+            if not row.size:
+                continue
+            fhand.write(sep)
+            fhand.write(sep.join(map(lambda x: f"{x:.5f}", row)))
+            fhand.write("\n")
+
 
 def calc_dists_from_pca(pca_result, metric="euclidean"):
     projections = pca_result["projections"]
     dists = scipy.spatial.distance.pdist(projections, metric=metric)
     return Distances(names=projections.index, dist_vector=dists)
+
+
+def test_to_mega():
+    dists = Distances(["a", "b", "c"], [0.5, 0.3, 0.2])
+    fhand = StringIO()
+    dists.write_mega(fhand)
+    text = fhand.getvalue()
+    assert "#a\n#b\n#c" in text
+    assert "   0.50000\n   0.30000   0.20000\n"
+
+
+if __name__ == "__main__":
+    test_to_mega()
+    dframe = pandas.read_csv(
+        open(
+            "candidate_root_genes.extended_2500pb.tier1_low_qual_085_vars.dists", "rt"
+        ),
+        sep="\t",
+        index_col=0,
+        header=0,
+    )
+    dists = Distances(dframe.columns, scipy.spatial.distance.squareform(dframe.values))
+    fhand = open(
+        "candidate_root_genes.extended_2500pb.tier1_low_qual_085_vars.meg", "wt"
+    )
+    print(dists.write_mega(fhand))
